e78a7cae9df2402283eecf2c6b10b173551b8c36
xaverstiensmeier@gmx.de <xaverstiensmeier@gmx.de>
2021-08-11 23:59:28 +0200

    Added die support. Added intext call support.

--- plotg.py
+++ plotg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description='Load files and generate adventure!')
 parser.add_argument('-l','--list', nargs='+', help='File pools', required=True)
@@ -8,13 +9,49 @@ parser.add_argument('-n','--number', type=int, help='Number of times a random en
 parser.add_argument('-s','--subfolder', help='Is used when the files are in a subfolder', default="")
 args = parser.parse_args()
 
+def bold(str):
+    return "\033[1m" + str + "\033[0m"
+
+def rollDie(n, sides, modificator):
+    summation = modificator
+    for elem in range(int(n)):
+        current = random.randint(1, int(sides))
+        summation+=current
+    return summation
+
+def parseDie(text):
+    modificator = 0
+    arguments = text.split("-")
+    if(len(arguments)>=2):
+        for elem in arguments[1:]:
+            modificator-=int(elem)
+        text = arguments[0]
+    else:
+        arguments = text.split("+")
+        if(len(arguments)>=2):
+            for elem in arguments[1:]:
+                modificator+=int(elem)
+            text = arguments[0]
+    if(text[0].isdigit()):
+        values = text.split("d")
+        return rollDie(values[0],values[1], modificator)
+
+def parse(text):
+    if(text[0].isdigit()):   
+        return parseDie(text)
+    else:
+        return getRListElement(text)
+    #raise ValueError("Brackets are for die roll only {2d6+2} for example is k {hans} is not.")
+
+def findToParse(text):
+    return re.sub(r"\{(.*?)\}", lambda match: "{0}".format(parse(match.group(1))), text)
 
 def loadFile(filename): # loads a file and makes a random choice
     with open("src/"+args.subfolder+filename) as file_in:
         lines = []
         line_weight = []
         for rawline in file_in:
-            line = rawline.rstrip("\n")
+            line = findToParse(rawline.rstrip("\n"))
             splitted = line.split("|")
             if(len(splitted) == 1):
                 lines.append(splitted[0])
@@ -28,7 +65,7 @@ def evaluate(elem, functionBound): # evaluates and creates string formating
     if(len(functionBound)>1): # is a function there? '_'
         return ("{:<7} : {:<10} - {}".format(elem,functionBound[0],doEntireListAsOne(functionBound[1:])))  
     else:
-        return ("{:<7} : {:<10}".format(elem, functionBound[0]))
+        return functionBound[0]
 
 def getRListElement(elem):
     result = loadFile(elem)
